620a5b36cfe9e0a88a58bc4ab0c969e6073ca1a2
lvyuanjiao <lvyuanjiao@yeah.net>
2016-01-08 17:01:47 +0800

    Updated to support sql-mapper@^1.1.0

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
 'use strict';
 
-var util = require('./util');
-var buildCacheProvider = require('./cache-provider');
+var LRU = require("lru-cache");
 
 /*
-  var opts = {
-    post: {
-      maxSize: 500,
-      maxAge: 1000 * 60 * 60
-    }
+  var opt = {
+    maxSize: 500,
+    maxAge: 1000 * 60 * 60
   }
 */
-module.exports = function(opts) {
-  var cacheProvider = buildCacheProvider(opts);
-
-  function getCacheProvider(plugin) {
-    return cacheProvider(plugin.params[0] || plugin.ctx.namespace);
-  }
-
-  function getCacheKey(plugin, params) {
-    return util.getCacheKey(plugin.ctx.dialect, plugin.ctx.database, plugin.queryName, params);
-  }
+module.exports = function(opt) {
+  opt = opt || {};
+  var cache = LRU({
+    max: opt.maxSize || 1024,
+    maxAge: opt.maxAge || 1000 * 60 * 60, // one hour
+    length: function(n, key) {
+      return n.length + key.length;
+    }
+  });
   return {
-    cache: {
-      onQuery: function(params, plugin, done) {
-        var cacheKey = plugin.ctx.cacheKey = getCacheKey(plugin, params);
-        done(getCacheProvider(plugin).get(cacheKey));
-      },
-      afterMapping: function(results, plugin, done) {
-        getCacheProvider(plugin).set(plugin.ctx.cacheKey, results);
-        done(results);
-      }
+    get: function(key) {
+      return cache.get(key);
     },
-    flush: {
-      onQuery: function(params, plugin, done) {
-        getCacheProvider(plugin).del(getCacheKey(plugin, params));
-        done();
-      }
+    set: function(key, value) {
+      cache.set(key, value);
+    },
+    reset: function() {
+      cache.reset();
     }
   };
 };
--- test/index.js
+++ test/index.js
@@ -1,97 +1,28 @@
 'use strict';
 
 var assert = require('assert');
-var util = require('../util');
-var getCachePlugin = require('../index');
+var getCache = require('../index');
 
 (function () {
 
-  console.log('Object check sum');
-  var nest = {
-    'y': 'yyyy',
-    'z': 'zzzz',
-    'x': 'xxxx'
-  };
-  var json1 = {
-    'b': 'bbbb',
-    'a': 'aaaa',
-    'd': 'dddd',
-    'c': nest
-  };
-  var json2 = {
-    'd': 'dddd',
-    'c': nest,
-    'a': 'aaaa',
-    'b': 'bbbb'
-  };
-
-  util.sortJSON(json1);
-  util.sortJSON(json2);
-  assert.deepEqual(json1, json2, 'json not the same');
-  console.log('  Sort JSON', 'pass');
-
-  assert.deepEqual(util.checksum(JSON.stringify(json1)), util.checksum(JSON.stringify(json2)), 'checksum not the same');
-  console.log('  Checksum', 'pass');
-
-
-  console.log('Array check sum');
-
-  nest = {
-    'y': 'yyyy',
-    'z': 'zzzz',
-    'x': 'xxxx'
-  };
-
-  var arr1 = ['c', 'a', nest, 'b'];
-  var arr2 = [nest, 'b', 'c', 'a'];
-
-  util.sortJSON(arr1);
-  util.sortJSON(arr2);
-  assert.deepEqual(arr1[2], arr2[0], 'array not the same');
-  console.log('  Sort JSON', 'pass');
-
-  assert.deepEqual(util.checksum(JSON.stringify(arr1[2])), util.checksum(JSON.stringify(arr2[0])), 'checksum not the same');
-  console.log('  Checksum', 'pass');
-
-
   console.log('Cache');
-  var cachePlugin = getCachePlugin({
-    'cache1': {
-      maxSize: 500,
-      maxAge: 1000 * 60 * 60
-    }
-  });
-  var pluginOpt = {
-    params: ['cache1'],
-    ctx: {
-      dialect: 'mysql',
-      database: 'test',
-      namespace: 'cache1',
-      queryName: 'cache1.select'
-    }
-  };
+  var cache = getCache();
 
-  var params = [0, 1];
-  var values = [0, 1, 2, 3, 4, 5];
+  var key = 'k';
+  var value = 'v';
 
-  cachePlugin.cache.onQuery(params, pluginOpt, function (results) {
-    assert.ok(results === undefined, 'cache not exists');
-    console.log('   cache not exists');
-    cachePlugin.cache.afterMapping(values, pluginOpt, function (results) {
-      assert.deepEqual(results, values, 'cache values not the same');
-      console.log('   cache values');
-      cachePlugin.cache.onQuery(params, pluginOpt, function (results) {
-        assert.deepEqual(results, values, 'cache values not the same');
-        console.log('   check cache values');
-        cachePlugin.flush.onQuery(params, pluginOpt, function (results) {
-          console.log('   flush cache');
-          cachePlugin.cache.onQuery(params, pluginOpt, function (results) {
-            assert.ok(results === undefined, 'cache not exists');
-            console.log('   cache values');
-          });
-        });
-      });
-    });
-  });
+  var v = cache.get(key);
+  assert.notEqual(value, v, 'the same');
+  console.log('  empty');
+
+  cache.set(key, value);
+  v = cache.get(key);
+  assert.equal(value, v, 'not the same');
+  console.log('  cache it');
+
+  cache.reset();
+  v = cache.get(key);
+  assert.notEqual(value, v, 'the same');
+  console.log('  reset the cache');
 
 })();
